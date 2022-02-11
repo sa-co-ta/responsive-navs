8a0f60f513be9359bf008fefb38081b4c5b837e7
sa-co-ta <95979890+sa-co-ta@users.noreply.github.com>
2022-02-11 14:02:30 +0000

    removes styles, adds desktop dialog

--- script3.js
+++ script3.js
@@ -1,5 +1,11 @@
 const mobileNav = document.querySelector(".mobile--container--three");
 const navIconThree = document.getElementById("icon--three");
+const dialog = document.getElementById("dialog");
+const navDialog = document.querySelector(".nav__dialog");
+
+dialog.addEventListener("click", () => {
+  navDialog.classList.toggle("show_dialog");
+});
 
 navIconThree.addEventListener("click", function (e) {
   mobileNav.classList.toggle("show");
